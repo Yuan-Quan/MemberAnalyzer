7f4481fad9356890549f250af67665cd1587bcfb
Luna <metroyuan@gmail.com>
2020-08-27 15:37:45 +0800

    added pac: ConfigManager, added some utils

---
+++ Utils.cs
@@ -0,0 +1,196 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Configuration;
+using System.Text.RegularExpressions;
+using System.Text;
+
+namespace MemberAnalyzer.Util
+{
+    public class Setting
+    {
+        public Setting(string key, string value,string description)
+        {
+            this.Key = key;
+            this.Value = value;
+            this.Description = description;
+        }
+
+        public string Key { get; set; }
+        public string Value { get; set; }
+        public string Description { get; set; }
+    }
+
+    public static class Util
+    {
+        private static byte[] ConvertStringToByteArray(string data)
+        {
+            return(new System.Text.UnicodeEncoding()).GetBytes(data);
+        }
+
+        private static System.IO.FileStream GetFileStream(string pathName)
+        {
+            return(new System.IO.FileStream(pathName, System.IO.FileMode.Open, 
+                        System.IO.FileAccess.Read, System.IO.FileShare.ReadWrite));
+        }
+
+        public static string GetSHA1Hash(string pathName)
+        {
+            string strResult = "";
+            string strHashData = "";
+
+            byte[] arrbytHashValue;
+            System.IO.FileStream oFileStream = null;
+
+            System.Security.Cryptography.SHA1CryptoServiceProvider oSHA1Hasher=
+                        new System.Security.Cryptography.SHA1CryptoServiceProvider();
+
+            try
+            {
+                oFileStream = GetFileStream(pathName);
+                arrbytHashValue = oSHA1Hasher.ComputeHash(oFileStream);
+                oFileStream.Close();
+
+                strHashData = System.BitConverter.ToString(arrbytHashValue);
+                strHashData = strHashData.Replace("-", "");
+                strResult = strHashData;
+            }
+            catch(System.Exception ex)
+            {
+                System.Console.WriteLine(ex.Message);
+            }
+
+            return(strResult);
+        }
+
+        /// <summary>
+        /// get settings!!
+        /// </summary>
+        /// <returns>Enumrable Settings</returns>
+        public static IEnumerable<Setting> GetAllSettings()
+        {
+            var appSettings = ConfigurationManager.AppSettings;
+
+            foreach (var key in appSettings.AllKeys)
+            {
+                yield return new Setting(key, appSettings[key].Split(',')[0], appSettings[key].Split(',')[1]);
+            }
+        }
+
+        /// <summary>
+        /// Print current settings
+        /// </summary>
+        public static void ReadAllSettings()
+        {
+            try
+            {
+                var appSettings = ConfigurationManager.AppSettings;
+
+                if (appSettings.Count == 0)
+                {
+                    Console.WriteLine("AppSettings is empty.");
+                }
+                else
+                {
+                    Console.WriteLine("Current app settings: ");
+                    foreach (var key in appSettings.AllKeys)
+                    {
+                        Console.WriteLine("Key: {0} Value: {1}", key, appSettings[key]);
+                    }
+                }
+            }
+            catch (ConfigurationErrorsException)
+            {
+                Console.WriteLine("Error reading app settings");
+            }
+        }
+
+        /// <summary>
+        /// Read a setting by key
+        /// </summary>
+        /// <param name="key">key</param>
+        /// <returns>value of appSettings[key]</returns>
+        public static string ReadSetting(string key)
+        {
+            try
+            {
+                var appSettings = ConfigurationManager.AppSettings;
+                string result = appSettings[key] ?? null;
+                return result;
+            }
+            catch (ConfigurationErrorsException)
+            {
+                Console.WriteLine("Error reading app settings");
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Add a setting in appsettings
+        /// </summary>
+        /// <param name="key">key</param>
+        /// <param name="value">value</param>
+        public static void AddUpdateAppSettings(string key, string value)
+        {
+            try
+            {
+                var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                var settings = configFile.AppSettings.Settings;
+                if (settings[key] == null)
+                {
+                    settings.Add(key, value);
+                }
+                else
+                {
+                    settings[key].Value = ""+value+","+settings[key].Value.Split(',')[1];
+                    
+                }
+                configFile.Save(ConfigurationSaveMode.Modified);
+                ConfigurationManager.RefreshSection(configFile.AppSettings.SectionInformation.Name);
+            }
+            catch (ConfigurationErrorsException)
+            {
+                Console.WriteLine("Error writing app settings");
+            }
+        }
+
+        /// <summary>
+        /// Reading and Echoing the File
+        /// <br></br>
+        /// Each time the calling code requests the next item from the sequence, the code reads the next line of text from the file and returns it.
+        /// </summary>
+        /// <param name="file">Path of the file</param>
+        /// <returns>string</returns>
+        public static IEnumerable<string> ReadFrom(string file)
+        {
+            string line;
+            using var reader = File.OpenText(file);
+            while ((line = reader.ReadLine()) != null)
+            {
+                yield return line;
+            }
+        }
+
+        public static void WriteAFile(List<string> ls, string path, string fileName)
+        {
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(path, fileName)))
+            {
+                foreach (string line in ls)
+                outputFile.WriteLine(line);
+            }
+
+            var preForegroundColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"File [{fileName}] Write Succeed!!");
+            Console.ForegroundColor = preForegroundColor;
+        }
+
+
+    }
+
+
+}
